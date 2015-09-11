7782fd8e1f446f00bf358a8be1d00cef4479f256
Huynh Quang Thao <huynhquangthao@gmail.com>
2015-09-11 11:32:21 +0700

    prepare code structure for DatNT implement.

--- Source/crawler/src/com/fpt/router/work/TimeCrawlerPipe.java
+++ Source/crawler/src/com/fpt/router/work/TimeCrawlerPipe.java
@@ -1,12 +1,25 @@
 package com.fpt.router.work;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Purpose:
  * Created by Huynh Quang Thao on 9/6/15.
  */
 public class TimeCrawlerPipe {
 
-    public void run() {
+    public Map<Integer, String> busTimeExcelLinks;
 
+    public TimeCrawlerPipe() {
+        busTimeExcelLinks = new HashMap<Integer, String>();
+    }
+
+    private void parseAllExcelLinks() {
+
+    }
+
+    public void run() {
+        parseAllExcelLinks();
     }
 }
