6d3dcabb89973dad6a9db19a74db0f9ae08f7c51
Huynh Quang Thao <huynhquangthao@gmail.com>
2015-10-19 11:40:06 +0700

    remove multidex

--- Source/RouterMobile/mobile/src/main/java/com/fpt/router/framework/RouterApplication.java
+++ Source/RouterMobile/mobile/src/main/java/com/fpt/router/framework/RouterApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.multidex.MultiDex;
 import android.util.Log;
 
 import com.fpt.router.service.GPSServiceOld;
