47f17b00f9023aedae6e2392d5d56e585e15e576
Huynh Quang Thao <huynhquangthao@gmail.com>
2015-10-21 13:45:12 +0700

    add journey model

---
+++ Source/RouterMobile/library/src/main/java/com/fpt/router/library/model/bus/Journey.java
@@ -0,0 +1,15 @@
+package com.fpt.router.library.model.bus;
+
+import org.joda.time.Period;
+
+import java.util.List;
+
+/**
+ * Created by Huynh Quang Thao on 10/21/15.
+ */
+public class Journey {
+    public Period totalTime;
+    public int minutes;
+    public double totalDistance;
+    public List<Result> results;
+}
