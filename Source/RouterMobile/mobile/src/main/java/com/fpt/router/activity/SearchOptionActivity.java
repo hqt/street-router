3fe1323937c9d5c5333c56f8b51c9b7af992938d
Huynh Quang Thao <huynhquangthao@gmail.com>
2015-10-25 16:51:32 +0700

    layout

--- Source/RouterMobile/mobile/src/main/java/com/fpt/router/activity/SearchOptionActivity.java
+++ Source/RouterMobile/mobile/src/main/java/com/fpt/router/activity/SearchOptionActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
@@ -42,6 +43,7 @@ public class SearchOptionActivity extends Activity {
         transferNumberSpinner = (Spinner) findViewById(R.id.number_spinner);
         optimizeCheckbox = (CheckBox) findViewById(R.id.checkBox);
         swapLocationButton = (ImageButton) findViewById(R.id.swap_location_btn);
+
 
         optimizeCheckbox.setChecked(SearchRouteActivity.optimize);
 
