ec35a902ed7f921b670d7dcb3f2d1156bb25b42a
Kunwejry <namntse61132@fpt.edu.vn>
2015-11-15 11:49:09 +0700

    update marker

--- Source/RouterMobile/mobile/src/main/java/com/fpt/router/fragment/base/NutiteqMapBaseFragment.java
+++ Source/RouterMobile/mobile/src/main/java/com/fpt/router/fragment/base/NutiteqMapBaseFragment.java
@@ -188,12 +188,8 @@ public class NutiteqMapBaseFragment extends Fragment implements OnChangedListene
             mapView.setTilt(30f, 0.5f);
             mapView.setZoom(24f, 05f);
             mapView.setMapRotation(360 - azimut, 0f);
-            if(marker != null) {
-                marker.setVisible(false);
-            }
             if(model != null) {
                 model.setRotation(new MapVec(0, 0, 1), 360-azimut);
-                model.setVisible(true);
             }
         } else {
             if (marker != null) {
